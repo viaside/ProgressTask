5654eb1208307f6c90b370e9f2904122d4af8a97
Viaside <ivanvoloshiin@mail.ru>
2023-05-04 15:26:30 +0300

    Update Progress.js

--- src/Progress.js
+++ src/Progress.js
@@ -11,8 +11,8 @@ class Progress {
     }
 
     start(element){
-        this.progressElement.setAttributeNS(null, 'style', '');
-
+        this.progressElement.setAttributeNS(null, 'style', 'width: 300; height: 300;');
+        
         var circleBg = document.createElementNS("http://www.w3.org/2000/svg", "circle");
         circleBg.setAttributeNS(null, 'cx', this.x);
         circleBg.setAttributeNS(null, 'cy', this.y);
